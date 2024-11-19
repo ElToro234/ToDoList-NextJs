fb3798d116ecf7a2673c98461889aadb79aa0bbe
Alexander Liu <alexanderliuxinlu@gmail.com>
2024-11-19 16:11:41 -0500

    8th commit

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,25 +1,39 @@
+"use client"; 
+
 import { ITask } from "@/types/tasks";
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+import Modal from "./Modal";
 
 interface TaskProps {
     task: ITask
 }
 const Task: React.FC <TaskProps> = ( {task} ) =>{
-  const[openModalEdit, setModalOpenEdit] = useState(false);
-  const[openModalDelete, setModalOpenDelete] = useState(false);
+  const[openModalEdit, setModalOpenEdit] = useState<boolean>(false);
+  const[openModalDelete, setModalOpenDelete] = useState<boolean>(false);
+  const[taskToEdit, setTaskToEdit] = useState<string>(task.text);
 
+  const handleSubmitEditTask: FormEventHandler <HTMLFormElement> = async(e) => {
+      e.preventDefault();
+      // await addTodo({
+      //     id: uuidv4(),
+      //     text: newTaskValue
+      // })
+      setTaskToEdit("");
+      setModalOpenEdit(false);
+      router.refresh();
+  };
     return <tr key = {task.id}>
     <td className="w-full">{task.text}</td>
     <td className='flex gap-5'>
-      <FaRegEdit className="text-blue-500" cursor= 'pointer' size = {15}/>
-        <Modal modalOpen = {modalOpen} setModalOpen = { setModalOpen } > 
-              <form onSubmit={ handleSubmitNewTask }>
-                  <h3 className="font-bold">Add new task</h3>
+      <FaRegEdit onClick= {() => setModalOpenEdit(true)} className="text-blue-500" cursor= 'pointer' size = {15}/>
+        <Modal modalOpen = {openModalEdit} setModalOpen = { setModalOpenEdit } > 
+              <form onSubmit={ handleSubmitEditTask }>
+                  <h3 className="font-bold">Edit task</h3>
                   <div className="modal-action">
                   <input 
-                      value={newTaskValue}
-                      onChange= {e => setNewTaskValue(e.target.value)}
+                      value={taskToEdit}
+                      onChange= {e => setTaskToEdit(e.target.value)}
                       type="text" 
                       placeholder="Type here" 
                       className="input input-bordered w-full" />
